4304bd8f28329e00a9a5ce54c54355a4951134ea
Dionne Rodriguez <DionneRodriguez.SE@gmail.com>
2019-07-30 12:29:30 -0400

    added user log in code

---
+++ config/database.js
@@ -0,0 +1,12 @@
+switch (expression) {
+  case expression:
+
+    break;
+  default:
+
+}// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://Rodriguez:KMvwi1gJfEJM7pnO@cluster0-egyk9.mongodb.net/test?retryWrites=true&w=majority", // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'rouletteApp'
+};
--- server.js
+++ server.js
@@ -1,83 +1,67 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-var db, collection;
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-const url = "mongodb+srv://Rodriguez:ig7teXOk4AQif33k@rpsls-app-9ds4b.mongodb.net/test?retryWrites=true&w=majority";
-const dbName = "UserBetMoney";
+var configDB = require('./config/database.js');
 
-app.listen(2000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
-    });
-});
+var db
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+// configuration ===============================================================
+mongoose.connect(configDB.url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  require('./app/routes.js')(app, passport, db);
+}); // connect to our database
+
+//app.listen(port, () => {
+    // MongoClient.connect(configDB.url, { useNewUrlParser: true }, (error, client) => {
+    //     if(error) {
+    //         throw error;
+    //     }
+    //     db = client.db(configDB.dbName);
+    //     console.log("Connected to `" + configDB.dbName + "`!");
+    //     require('./app/routes.js')(app, passport, db);
+    // });
+//});
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-//gets html template
-app.get('/', (req, res) => {
-  db.collection('bet').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {bet: result})
-     console.log({bet: result})
-  })
-})
+app.set('view engine', 'ejs'); // set up ejs for templating
 
- app.post('/input', (req, res) => {
-   var betValue = parseInt(req.body.userBet)
-   console.log(betValue)
-   db.collection('bet').save({bet: betValue}, (err, result) => {
-   if (err) return console.log(err)
-   console.log('saved to database')
-   res.redirect('/')
-  })
-})
+// required for passport
+app.use(session({
+    secret: 'rcbootcamp2019a', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 
-app.put('/lossBet', (req, res) => {
-  db.collection('bet')
-  .findOneAndUpdate({bet: req.body.bet}, {
-    $set: {
-      bet: req.body.bet - req.body.amount
-    }
-  },{
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+// routes ======================================================================
+//require('./app/routes.js')(app, passport, db); // load our routes and pass in our app and fully configured passport
 
-app.put('/winBet', (req, res) => {
-  db.collection('bet')
-  .findOneAndUpdate({bet: req.body.bet}, {
-    $set: {
-      bet:req.body.bet + req.body.amount * 10 
-    }
-  },{
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-
-
-app.delete('/bet', (req, res) => {
-  console.log('CALLED')
-  db.collection('bet').findOneAndDelete({bet: parseInt(req.body.bet)}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send({message:'Message deleted!'})
-  })
-})
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
